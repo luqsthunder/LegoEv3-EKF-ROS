41fa46cdf4a5294f9396d30586539b5347608722
Bruno Lima <bg.lima@hotmail.com>
2018-11-14 17:29:44 -0300

    Documented test procedures

--- ev3_controller/test/state_machine_test.py
+++ ev3_controller/test/state_machine_test.py
@@ -2,10 +2,30 @@ import rospy
 import math
 from math import sin, cos, pi
 import tf
-from geometry_msgs.msg import Quaternion,Pose,PoseWithCovarianceStamped
+from geometry_msgs.msg import Quaternion, Pose, PoseWithCovarianceStamped
 
+"""
+This test routine fakes an odometry, sending poses to /odom_combined topic.
+It's purpose is to test ev3_controller state machine responses, simulating a
+robot_pose_ekf output.
+"""
 
 def create_pose_with_covariance_msg(x, y, theta):
+    """
+    Create and returns a message of robot_pose_ekf output class.
+
+    @params
+    x : float
+        x position in m.
+    y : float
+        y position in m.
+    theta : float
+        z orientation in rad.
+
+    @returns
+    geometry_msgs/PoseWithCovarianceStamped
+        Pose with positions and orientation set.
+    """
     msg = PoseWithCovarianceStamped()
     msg.pose.pose.position.x = x
     msg.pose.pose.position.y = y
@@ -21,19 +41,22 @@ def create_pose_with_covariance_msg(x, y, theta):
 if __name__ == '__main__':
     pub = rospy.Publisher('/odom_combined', PoseWithCovarianceStamped, queue_size=1)
     rospy.init_node('state_machine_tester')
-    period = rospy.Duration(5)
+    # Half side length of square to be performed
     half_side = 0.5
-
+    # Array that will contain poses messages to be faked
     msg_array = []
     msg_array.append( create_pose_with_covariance_msg(  half_side,  half_side,  math.pi) )
     msg_array.append( create_pose_with_covariance_msg( -half_side,  half_side,  -math.pi/2 ) )
     msg_array.append( create_pose_with_covariance_msg( -half_side, -half_side,  0 ) )
     msg_array.append( create_pose_with_covariance_msg(  half_side, -half_side,  math.pi/2 ) )
-
+    # Time in seconds between two messages
+    period = rospy.Duration(5)
+    # Iterate over pose array
     for (i, msg) in enumerate(msg_array):
+        # Check if roscore is still active
         if rospy.is_shutdown():
             break
-
+        # Publish and wait next message time
         rospy.loginfo("Publishing message of index {}".format(i))
         pub.publish(msg)
         rospy.sleep(period)
